09e6b655becd608f2c5dce863334590e28bb1a66
JP <jartavias@ucenfotec.ac.cr>
2022-10-23 13:01:54 -0600

    Room1 inicial

--- core/src/com/bisoft/game/StarGame.java
+++ core/src/com/bisoft/game/StarGame.java
@@ -13,7 +13,8 @@ public class StarGame extends Game {
 	public void create() {
 		Render.Batch = new SpriteBatch();
 		//Resources.MAIN.setScreen(new LoreScreen());
-		Resources.MAIN.setScreen(new LoadScreen());
+
+		Resources.MAIN.setScreen(new Room1Screen());
 
 		//Resources.MAIN.setScreen(new MenuScreen());
 	}
