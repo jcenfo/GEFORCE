cde14ef7feeb1d9b7312d7a2f75c79e9499c31cb
Jonatan <jgura@ucenfotec.ac.cr>
2022-11-29 00:05:03 -0600

    Fix path for tsx and tmx

--- core/src/com/bisoft/game/screen/RoomMountain.java
+++ core/src/com/bisoft/game/screen/RoomMountain.java
@@ -36,7 +36,7 @@ public class RoomMountain implements Screen {
 
     public RoomMountain() {
         input = new Inputs();
-        screen = new Pantalla("rooms/city/RoomDelTiempo.tmx");
+        screen = new Pantalla("rooms/city/RoomAlcantarilla.tmx");
         ///this.statusText = new StatusText(true);
         Resources.CURRENT_LOCATION = "City";
         int[] layers = {1, 3};
